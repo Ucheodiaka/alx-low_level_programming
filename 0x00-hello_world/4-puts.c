7b1245d33677fba045505c739e797e19990b1904
Ucheodiaka <ucheodiaka@gmail.com>
2022-09-08 22:05:22 +0100

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,9 +7,8 @@
 * this program prints "Programming is like building a multilingual puzzle
 * Return: 0
 */
-
 int main(void)
 {
-	put("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0); 
 }
