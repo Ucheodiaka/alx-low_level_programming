8b699e2f59c3796671abffb4048e0081eea5e9bf
Ucheodiaka <ucheodiaka@gmail.com>
2022-09-10 18:48:43 +0100

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - lowercase and uppercase
+*
+* Return: Always (Success)
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+	putchar(ch);
+}
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+	putchar(ch);
+}
+putchar('\n');
+return (0);
+}
