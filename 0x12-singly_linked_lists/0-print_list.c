81da269bf1dc71ed5b7b95062ba1737b3d69405f
Ucheodiaka <ucheodiaka@gmail.com>
2022-10-20 23:24:52 +0100

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,9 +6,9 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t nodelen;
+	size_t nelem;
 
-	nodelen = 0;
+	nelem = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -16,7 +16,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		nodelen++;
+		nelem++;
 	}
-	return (nodelen);
+	return (nelem);
 }
